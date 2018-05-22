a606b849fbd82046472958284e952bcfa39eda97
th3eesome <610870693@qq.com>
2018-05-22 22:15:13 +0800

    table id

--- List.js
+++ List.js
@@ -43,6 +43,13 @@ export default class ExaminationView extends Component {
 
     componentWillMount() {
        // console.log(this.itemId);
+        this.table46Id(this.allQuestions[3][6]);
+        this.table51Id(this.allQuestions[4][0]);
+        this.table53Id(this.allQuestions[4][3]);
+        this.table54Id(this.allQuestions[4][4]);
+        this.table614Id(this.allQuestions[5][23]);
+        this.table78Id(this.allQuestions[6][12]);
+        this.table715Id(this.allQuestions[6][25]);
         console.log(this.props.navigation.getParam('id', 'NO-ID'));
         const id = this.props.navigation.getParam('id', 'NO-ID');
         if (id !== 'NO-ID') {
@@ -102,22 +109,6 @@ export default class ExaminationView extends Component {
                         break;
                     }
                 }
-                /*if (question.type === 'table') {
-                    answerBucket[child].push({
-                        "Record_ID": question.id,
-                        "Record_Value": this.generateTable(question)
-                    });
-                } else if (question.type === 'table_2') {
-                    answerBucket[child].push({
-                        "Record_ID": question.id,
-                        "Record_Value": this.generateTable_2(question)
-                    });
-                } else {
-                    answerBucket[child].push({
-                        "Record_ID": question.id,
-                        "Record_Value": ''
-                    })
-                }*/
             })
         }
         console.log(answerBucket);
@@ -128,6 +119,166 @@ export default class ExaminationView extends Component {
             console.log(this.state);
             console.log(this.virtualState);
         })
+    }
+
+    table46Id(question) {
+        const config = question.configuration;
+        const IdArray = [];
+        for (let row = 0; row < config.column_title.length; row++) {
+            IdArray.push([]);
+            IdArray[row].push({
+                "Record_ID": `ID4_6_${row}`,
+                "Record_Value": config.column_title[row]
+            });
+            for (let col = 1; col<config.header.length; col++) {
+                IdArray[row].push({
+                    "Record_ID": `ID4_6_${String.fromCharCode((97 + row))}`,
+                    "Record_Value": ''
+                })
+            }
+        }
+        console.log(IdArray);
+    }
+
+    table51Id(question) {
+        const config = question.configuration;
+        const IdArray = [];
+        for (let row = 0; row < config.column_title.length; row++) {
+            IdArray.push([]);
+            IdArray[row].push({
+                "Record_ID": `ID5_1_${row + 1}_0`,
+                "Record_Value": config.column_title[row]
+            });
+            for (let col = 1; col<config.header.length - 1; col++) {
+                IdArray[row].push({
+                    "Record_ID": `ID5_1_${row + 1}_${col}`,
+                    "Record_Value": false
+                })
+            }
+            IdArray[row].push({
+                "Record_ID": `ID5_1_${row + 1}_b`,
+                "Record_Value": ''
+            })
+        }
+        IdArray[29][0].Record_ID = 'ID5_1_30_o';
+        IdArray[29][0].Record_Value = '';
+        console.log(IdArray);
+    }
+
+    table53Id(question) {
+        const config = question.configuration;
+        const IdArray = [];
+        for (let row = 0; row < config.column_title.length; row++) {
+            IdArray.push([]);
+            IdArray[row].push({
+                "Record_ID": `ID5_3_${row}_0`,
+                "Record_Value": config.column_title[row]
+            });
+            for (let col = 1; col<config.header.length; col++) {
+                IdArray[row].push({
+                    "Record_ID": `ID5_3_${row+1}`,
+                    "Record_Value": ''
+                })
+            }
+        }
+        IdArray[2][1].Record_ID = 'ID5_3_4';
+        console.log(IdArray);
+    }
+
+    table54Id(question) {
+        const config = question.configuration;
+        const IdArray = [];
+        for (let row = 0; row < config.column_title.length; row++) {
+            IdArray.push([]);
+            IdArray[row].push({
+                "Record_ID": `ID5_4_${row + 1}_0`,
+                "Record_Value": config.column_title[row]
+            });
+            for (let col = 1; col<config.header.length; col++) {
+                IdArray[row].push({
+                    "Record_ID": `ID5_4_${row + 1}_${col}`,
+                    "Record_Value": false
+                })
+            }
+        }
+        console.log(IdArray);
+    }
+
+    table614Id(question) {
+        const config = question.configuration;
+        const IdArray = [];
+        for (let row = 0; row < config.column_title.length; row++) {
+            IdArray.push([]);
+            IdArray[row].push({
+                "Record_ID": `ID6_14_${row + 1}_0`,
+                "Record_Value": config.column_title[row]
+            });
+            IdArray[row].push({
+                "Record_ID": `ID6_14_${row + 1}`,
+                "Record_Value": false,
+            });
+            IdArray[row].push({
+                "Record_ID": `ID6_14_${String.fromCharCode(97+row)}`,
+                "Record_Value": '',
+            });
+        }
+        console.log(IdArray);
+    }
+
+    table78Id(question) {
+        const config = question.configuration;
+        const IdArray = [];
+        for (let row = 0; row < config.column_title.length; row++) {
+            IdArray.push([]);
+            IdArray[row].push({
+                "Record_ID": `ID7_8_0_${row+1}`,
+                "Record_Value": config.column_title[row]
+            });
+            for (let col = 1; col < config.header.length; col++) {
+                if(config.column_type[col] === 'check') {
+                    IdArray[row].push({
+                        "Record_ID": `ID7_8_${String.fromCharCode(col+96)}_${row+1}`,
+                        "Record_Value": false
+                    })
+                } else {
+                    IdArray[row].push({
+                        "Record_ID": `ID7_8_${String.fromCharCode(col+96)}_${row+1}`,
+                        "Record_Value": ''
+                    })
+                }
+            }
+        }
+        IdArray[25][0].Record_ID = 'ID7_8_h_26';
+        IdArray[25][0].Record_Value = '';
+        IdArray[26][0].Record_ID = 'ID7_8_h_27';
+        IdArray[26][0].Record_Value = '';
+        console.log(IdArray);
+    }
+
+    table715Id(question) {
+        const config = question.configuration;
+        const IdArray = [];
+        for (let row = 0; row < config.column_title.length; row++) {
+            IdArray.push([]);
+            IdArray[row].push({
+                "Record_ID": `ID7_15_${String.fromCharCode(row+97)}_0`,
+                "Record_Value": config.column_title[row]
+            });
+            for (let col = 1; col < config.header.length; col++) {
+                if(config.column_type[row] === 'check') {
+                    IdArray[row].push({
+                        "Record_ID": `ID7_15_${String.fromCharCode(row+97)}_${col}`,
+                        "Record_Value": false
+                    })
+                } else {
+                    IdArray[row].push({
+                        "Record_ID": `ID7_15_${String.fromCharCode(row+97)}_${col}`,
+                        "Record_Value": ''
+                    })
+                }
+            }
+        }
+        console.log(IdArray);
     }
 
     handleChange(index, answer) {
@@ -164,20 +315,21 @@ export default class ExaminationView extends Component {
         for (let row = 0; row< config.column_title.length; row ++) {
             tableAnswer.push([]);
             for (let col = 0; col < config.column_type.length; col ++) {
+
                 if (config.column_type[col] === 'check') {
                     tableAnswer[row].push({
-                        "Record_ID": `${question.id}_${row}${col}`,
+                        "Record_ID": this.switchTableID(question.id, row, col),
                         "Record_Value": false
                     })
                 } else if (config.column_type[col] === 'text'){
                     tableAnswer[row].push({
-                        "Record_ID":  `${question.id}_${row}${col}`,
+                        "Record_ID":  this.switchTableID(question.id, row, col),
                         "Record_Value": config.column_title[row]
                     })
                 }
                 else {
                     tableAnswer[row].push({
-                        "Record_ID": `${question.id}_${row}${col}`,
+                        "Record_ID": this.switchTableID(question.id, row, col),
                         "Record_Value": ''
                     })
                 }
@@ -227,6 +379,15 @@ export default class ExaminationView extends Component {
         return `ID${newId}`;
     }
 
+    switchTableID(idstr, row, column) {
+        const idArray = idstr.split('.');
+        const columnLetter = String.fromCharCode((97 + row));
+        idArray.push(column);
+        idArray.push(columnLetter);
+        const newId = idArray.join('_');
+        return `ID${newId}`;
+    }
+
     setContent = () => {
         console.log(this.virtualState.answers[0][0].Record_Value);
         const saveId = this.virtualState.answers[0][0].Record_Value;
@@ -244,6 +405,8 @@ export default class ExaminationView extends Component {
         }
 
     };
+
+
 
     render() {
         const { params } = this.props.navigation.state;
